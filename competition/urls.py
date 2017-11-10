f6485b16bd126d4464e8b605159175fefc38dc1a
lafferc <lafferc@tcd.ie>
2017-11-10 22:50:36 +0000

    fixed #2

--- competition/urls.py
+++ competition/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<tour_name>\w+)/$', views.submit, name='submit'),
-    url(r'^(?P<tour_name>\w+)/predictions/$', views.predictions, name='predictions'),
-    url(r'^(?P<tour_name>\w+)/table/$', views.table, name='table'),
-    url(r'^(?P<tour_name>\w+)/join/$', views.join, name='join'),
+    url(r'^(?P<tour_name>[^/]+)/$', views.submit, name='submit'),
+    url(r'^(?P<tour_name>[^/]+)/predictions/$', views.predictions, name='predictions'),
+    url(r'^(?P<tour_name>[^/]+)/table/$', views.table, name='table'),
+    url(r'^(?P<tour_name>[^/]+)/join/$', views.join, name='join'),
 ]
