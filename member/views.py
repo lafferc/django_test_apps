bb9b2b17d72cc4a8677b7965b4743734280f748c
lafferc <lafferc@tcd.ie>
2018-01-30 21:17:57 +0000

    added templates and urls

--- member/views.py
+++ member/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.contrib.auth.decorators import login_required, permission_required
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    template = loader.get_template('profile.html')
+    current_site = get_current_site(request)
+    context = {
+        'site_name': current_site.name,
+    }
+    return HttpResponse(template.render(context, request))
+
+
--- server/urls.py
+++ server/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/', include('member.urls', namespace="member")),
+
 ]
