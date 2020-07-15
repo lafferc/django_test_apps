0af6a4c7a75b38892977b53a609121ed5e5fdb86
lafferc <lafferc@tcd.ie>
2020-07-15 22:44:00 +0100

    Added register tests

--- server/tests.py
+++ server/tests.py
@@ -2,12 +2,14 @@ from django.contrib.auth.models import User
 from django.core import mail
 from django.test import TestCase
 import unittest
+import re
+#import pdb; pdb.set_trace()
 
 class ServerViewTest (TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        #print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        #print("setuptestdata: run once to set up non-modified data for all class methods.")
         test_user1 = User.objects.create_user(username='testuser1', password='test123')
         test_user1.save()
 
@@ -17,6 +19,7 @@ class ServerViewTest (TestCase):
 
     def test_index_logged_in(self):
         login = self.client.login(username='testuser1', password='test123')
+        self.assertTrue(login)
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
@@ -24,10 +27,9 @@ class ServerViewTest (TestCase):
         response = self.client.get('/register/')
         self.assertEqual(response.status_code, 200)
 
-    @unittest.skip("TODO")
     def test_activate_logged_out(self):
         response = self.client.get('/activate/')
-        self.assertRedirects(response, '/accounts/login/?next=/')
+        self.assertEqual(response.status_code, 404)
 
     def test_about_logged_out(self):
         response = self.client.get('/about/')
@@ -39,21 +41,125 @@ class ServerViewTest (TestCase):
 
 
 class SignupTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_user1 = User.objects.create_user(username='testuser1',
+                                              password='test123',
+                                              email='test@example.com')
+        test_user1.save()
+
     def test_signup(self):
         response = self.client.post('/register/', {
             'username': 'new_user',
             'password1': 'password',
             'password2': 'password',
             'email': 'new_user@example.com',
-            'cookie_consent': 0,
-            'display_name_format': 0,
+            'cookie_consent': 1,
+            'display_name_format': 1,
             })
 
         self.assertRedirects(response, '/register/activation_sent/')
 
+        user = User.objects.get(username='new_user')
+
+        self.assertEqual(user.email, 'new_user@example.com')
+        self.assertFalse(user.is_active)
+        self.assertEqual(user.profile.cookie_consent, 1)
+        self.assertEqual(user.profile.display_name_format, 1)
+
         # Test that one message has been sent.
         self.assertEqual(len(mail.outbox), 1)
 
-        self.assertEqual(mail.outbox[0].subject, 'Activate Your Account')
-        self.assertEqual(mail.outbox[0].to, ['new_user@example.com'])
+        email = mail.outbox[0]
+
+        self.assertEqual(email.subject, 'Activate Your Account')
+        self.assertEqual(email.to, ['new_user@example.com'])
+
+        login = self.client.login(username='new_user', password='password')
+        self.assertFalse(login)
+
+        m = re.search('https?://testserver(/.*/)', email.body)
+        self.assertIsNotNone(m)
+
+        response = self.client.get(m.group(1))
+        self.assertRedirects(response, '/')
+
+        user = User.objects.get(username='new_user')
+        self.assertTrue(user.is_active)
+
+        login = self.client.login(username='new_user', password='password')
+        self.assertTrue(login)
+
+    @unittest.skip("500 server error")
+    def test_signup_duplicate_username(self):
+
+        user = User.objects.get(username='testuser1')
+
+        response = self.client.post('/register/', {
+            'username': 'testuser1',
+            'password1': 'password',
+            'password2': 'password',
+            'email': 'new_user@example.com',
+            'cookie_consent': 1,
+            'display_name_format': 1,
+            })
+
+        self.assertEqual(len(mail.outbox), 0)
+
+        login = self.client.login(username='testuser1', password='password')
+        self.assertFalse(login)
+
+    def test_signup_duplicate_email(self):
+        user = User.objects.get(username='testuser1')
+        self.assertEqual(user.email, 'test@example.com')
+
+        response = self.client.post('/register/', {
+            'username': 'new_user',
+            'password1': 'password',
+            'password2': 'password',
+            'email': 'test@example.com',
+            'cookie_consent': 1,
+            'display_name_format': 1,
+            })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'registration/register.html')
+
+        self.assertEqual(len(mail.outbox), 0)
+
+        try:
+            user = User.objects.get(username='new_user')
+        except User.DoesNotExist:
+            user = None
+
+        self.assertIsNone(user)
+
+        login = self.client.login(username='new_user', password='password')
+        self.assertFalse(login)
+
+    def test_signup_password_missmatch(self):
+        response = self.client.post('/register/', {
+            'username': 'new_user',
+            'password1': 'password1',
+            'password2': 'password2',
+            'email': 'new_user@example.com',
+            'cookie_consent': 1,
+            'display_name_format': 1,
+            })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'registration/register.html')
+
+        self.assertEqual(len(mail.outbox), 0)
+
+        try:
+            user = User.objects.get(username='new_user')
+        except User.DoesNotExist:
+            user = None
+
+        self.assertIsNone(user)
+
+        login = self.client.login(username='new_user', password='password1')
+        self.assertFalse(login)
 
