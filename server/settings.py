1039f99e524d8d274cc803b3becae6fcee911daf
lafferc <lafferc@tcd.ie>
2021-02-07 14:31:24 +0000

    Redirect admin logout to main login page after logout

--- server/settings.py
+++ server/settings.py
@@ -205,6 +205,8 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Provider specific settings SOCIALACCOUNT_PROVIDERS = { 'google': { # For each OAuth based provider, either add a ``SocialApp`` # (``socialaccount`` app) containing the required client # credentials, or list them here: 'APP': { 'client_id': '123', 'secret': '456', 'key': '' } } }
 
 # AllAuth settings
